8972b2811091ac06717bfaad95cba645cf322dea
Taha Afzal <tahaafzal5@hotmail.com>
2022-12-25 12:15:02 -0600

    Add RightSide

---
+++ src/CopyrightInfo/CopyrightInfo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CopyrightInfo() {
+    return (
+        <section className="copyright-info">
+            Copyright © 2022 Taha Afzal. All rights reserved.
+        </section>
+    );
+}
+
+export default CopyrightInfo;
