78941153b68dfd4ad304668b7cccdffb4c3488bc
Taha Afzal <tahaafzal5@hotmail.com>
2022-12-25 14:00:16 -0600

    Remove Home from Navbar

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,4 @@
 import Navbar from '../Navbar/Navbar.js';
-import Home from '../Home/Home.js';
 import Resume from '../Resume/Resume.js';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -10,7 +9,7 @@ function App() {
       <div className="app">
         <Navbar />
         <Routes>
-          <Route exact path='/' element={<Home />}></Route>
+          <Route exact path='/' element={<Resume />}></Route>
           <Route exact path='/resume' element={<Resume />}></Route>
         </Routes>
       </div>
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -5,7 +5,6 @@ import { Link } from "react-router-dom";
 function Navbar() {
     return (
         <section className="navbar">
-            <Link className="navbar-link" to="/">Home</Link>
             <Link className="navbar-link" to="/resume">Resume</Link>
         </section>
     );
