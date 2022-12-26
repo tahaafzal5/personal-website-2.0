47fc26ee19919510ddd32fff9ae402724482036c
Taha Afzal <tahaafzal5@hotmail.com>
2022-12-25 23:42:08 -0600

    add release notes

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,7 @@
 import Navbar from '../Navbar/Navbar.js';
 import Home from '../Home/Home.js';
 import Resume from '../Resume/Resume.js';
+import ReleaseNotes from '../ReleaseNotes/ReleaseNotes.js';
 import NotFound from '../NotFound/NotFound.js';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
@@ -13,6 +14,7 @@ function App() {
         <Route exact path='/' element={<Home />}></Route>
         <Route exact path='/home' element={<Home />}></Route>
         <Route exact path='/resume' element={<Resume />}></Route>
+        <Route exact path='/release-notes' element={<ReleaseNotes />}></Route>
         <Route path='*' element={<NotFound />}></Route>
       </Routes>
     </div>
