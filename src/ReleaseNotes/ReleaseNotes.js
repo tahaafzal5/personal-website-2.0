36a0711672ce6ca36ebf46512ffb45c6414b2d1e
Taha Afzal <tahaafzal5@hotmail.com>
2022-12-25 23:42:08 -0600

    add release notes

---
+++ src/ReleaseNotes/ReleaseNotes.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./ReleaseNotes.css";
+import Helmet from "react-helmet";
+
+function ReleaseNotes() {
+    return (
+        <section className="release-notes">
+            <Helmet>
+                <title>Release Notes - Taha Afzal</title>
+            </Helmet>
+
+            <div className="heading">
+                Release Notes
+            </div>
+
+            <info>
+                Newest to oldest in this format: <code>year.quarter.patch</code>
+                <ul>
+                    <li><code>year</code>: calendar year of release</li>
+                    <li><code>quarter</code>: quarter of release
+                        <ul>
+                            <li>Q1 (Jan - Mar)</li>
+                            <li>Q2 (Apr - Jun)</li>
+                            <li>Q3 (Jul - Sep)</li>
+                            <li>Q4 (Oct - Dec)</li>
+                        </ul>
+                    </li>
+                    <li><code>patch</code>: 0-indexed patch number of release</li>
+                </ul>
+            </info>
+
+            <div>
+                <p className="version">v22.4.0</p>
+                <ul>
+                    <li>First launch with just the good ol' navbar with a in-progress home page and a link to my resume page.</li>
+                    <li>404 page so you don't have to see GitHub's default 404 page.</li>
+                    <li>Release notes page for the ones who find it.</li>
+                </ul>
+            </div>
+        </section>
+    );
+}
+
+export default ReleaseNotes;
