b67008f54a2d3d987914444f65d8355f6215bad1
Taha Afzal <tahaafzal5@hotmail.com>
2022-12-26 00:08:27 -0600

    make the home page presentable

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -12,7 +12,42 @@ function Home() {
 
             <ProfileOverview />
 
-            
+            <div className="heading">
+                Introduction 👋🏽
+            </div>
+
+            <div className="content">
+                <p>By now, it is likely that you know my name and what I do already, so let's jump in.</p>
+                <p>Writing intros isn't easy. Thanks for your patience as I work on it. 👨🏽‍💻</p>
+            </div>
+
+            <div className="heading">
+                Why this website?
+            </div>
+
+            <div className="content">
+                <p>There are a lot of reasons, but the main one is to have a place where I can share my thoughts and ideas with the world.</p>
+                <p>It already has:</p>
+                <ul>
+                    <li>This home page which tells you <i>little</i> about me</li>
+                    <li><a href="/#/resume" target="_blank" rel="noreferrer">Resume page</a> that tells you some more about me, but mainly about my work.</li>
+                </ul>
+            </div>
+
+            <div className="heading">
+                What's next? ⏭️
+            </div>
+
+            <div className="content">
+                <ul>
+                    <li>Writings about things I like.</li>
+                    <li>Collection of things I find cool.</li>
+                    <li>How-To's and just general notes about things like <code>git, bash, vim</code>, etc. Mostly for myself but hopefully others can benefit too.</li>
+                    <li>And more... 🤷🏽‍♂️</li>
+                </ul>
+            </div>
+
+
         </section>
     );
 }
