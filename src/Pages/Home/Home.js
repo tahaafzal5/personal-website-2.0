5394b3d0c747d771611f230848a0c8fb520fda6d
Taha Afzal <tahaafzal5@hotmail.com>
2022-12-26 13:11:11 -0600

    fixing typos & updating release notes.

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -29,7 +29,7 @@ function Home() {
                 <p>There are a lot of reasons, but the main one is to have a place where I can share my thoughts and ideas with the world.</p>
                 <p>It already has:</p>
                 <ul>
-                    <li>This home page which tells you <i>little</i> about me</li>
+                    <li>This home page which tells you <i>little</i> about me.</li>
                     <li><a href="/#/resume" target="_blank" rel="noreferrer">Resume page</a> that tells you some more about me, but mainly about my work.</li>
                 </ul>
             </div>
@@ -42,7 +42,7 @@ function Home() {
                 <ul>
                     <li>Writings about things I like.</li>
                     <li>Collection of things I find cool.</li>
-                    <li>How-To's and just general notes about things like <code>git, bash, vim</code>, etc. Mostly for myself but hopefully others can benefit too.</li>
+                    <li>How-Tos and just general notes about things like <code>git, bash, vim</code>, etc. Mostly for myself but hopefully others can benefit too.</li>
                     <li>And more... 🤷🏽‍♂️</li>
                 </ul>
             </div>
