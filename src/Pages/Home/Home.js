c85d6abec89dd69cac7408b794bff2cce129c801
Taha Afzal <tahaafzal5@hotmail.com>
2023-01-02 21:57:15 -0600

    modify sentence to remove redundancy

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -17,7 +17,7 @@ function Home() {
             </div>
 
             <div className="content">
-                <p>By now, it is likely that you know my name and what I do already, so let's jump in.</p>
+                <p>By now, it is likely that you know my name and what I do, so let's jump in.</p>
                 <p>Writing intros isn't easy. Thanks for your patience as I work on it. 👨🏽‍💻</p>
             </div>
 
