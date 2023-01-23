5280d9f4890b2e5b2668de65a2b0882fd769aa10
Taha Afzal <tahaafzal5@hotmail.com>
2023-01-22 22:13:27 -0600

    Updates

---
+++ static/js/react-syntax-highlighter_languages_refractor_ini.6d7c9087.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpersonal_website_2_0=self.webpackChunkpersonal_website_2_0||[]).push([[6495],{3409:function(e){function t(e){e.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}e.exports=t,t.displayName="ini",t.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini.6d7c9087.chunk.js.map
