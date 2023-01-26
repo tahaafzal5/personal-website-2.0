cca40c61b353574712b7f0ff809288775776d60e
Taha Afzal <tahaafzal5@hotmail.com>
2023-01-25 19:32:53 -0600

    Updated Resume File and Webpage

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -7,35 +7,45 @@ function Projects() {
             <h1 className="heading">PROJECTS</h1>
 
             <Project
-                project="Ridesio"
-                role="Developer"
-                date="Oct 2020 - Present"
-                description={[
-                    "Developing a ride-sharing iOS app for college campuses over 6-weeks to serve as a communal and sustainable way for long-distance travel.",
-                    "Collaborating remotely in a team of 4 on the front and back ends of the app to implement MapKit and Parse.",
-                    "Focused on the UI and practicality of the app to secure 2nd position in CodePath's Demo Day and SF Student Showcase.",
-                ]}
-            />
-
-            <Project
-                project="Sanford HealthHack"
+                project="Health Memo"
                 role="Tech Lead"
                 date="Oct 2020 - Nov 2020"
                 description={[
                     "Led a team of 3 to develop an iOS app to help the elderly manage their chronic diseases and share regular updates with their doctor.",
                     "Utilized EMRs and implemented Google's Text-to-Speech and Speech-to-Text to create a personalized experience for patients.",
-                    "Won $9,000 along with an invitation to work with engineers at Sanford Health for further research and development of the project.",
+                    "Won $9,000 along with an invitation to work with engineers at Sanford Health for further research and development of the project."
                 ]}
             />
 
             <Project
-                project="CodePath"
+                project="Ridesio"
                 role="Developer"
-                date="Sep 2020 - Present"
+                date="Oct 2020 - Nov 2020"
                 description={[
-                    "Developing a social media app for college students over 6-weeks to help them connect with each other and find events on campus.",
-                    "Collaborating remotely in a team of 4 on the front and back ends of the app to implement MapKit and Parse.",
-                    "Focused on the UI and practicality of the app to secure 2nd position in CodePath's Demo Day and SF Student Showcase.",
+                    "Developed a ride-sharing iOS app for college campuses over 6-weeks to serve as a communal and sustainable way for long-distance travel.",
+                    "Collaborated remotely in a team of 4 to develop the app utilizing AWS, MapKit, SwiftUI, and Parse.",
+                    "Focused on the UI and practicality of the app to secure 2nd position in CodePath's Demo Day and SF Student Showcase."
+                ]}
+            />
+
+            <Project
+                project="Apple Beta Software Program"
+                role="Volunteer"
+                date="Jun 2020 - Sep 2020"
+                description={[
+                    "Developed a ride-sharing iOS app for college campuses over 6-weeks to serve as a communal and sustainable way for long-distance travel.",
+                    "Collaborated remotely in a team of 4 to develop the app utilizing AWS, MapKit, SwiftUI, and Parse.",
+                    "Focused on the UI and practicality of the app to secure 2nd position in CodePath's Demo Day and SF Student Showcase."
+                ]}
+            />
+
+            <Project
+                project="Midwest Undergraduate Data Analytics Competition (MUDAC)"
+                role="Team Lead"
+                date="Mar 2020 - Apr 2020"
+                description={[
+                    "Led Augustana University's team in MUDAC 2020 - a rigorous and prestigious data analytics hackathon.",
+                    "Predicted the outcomes of legal cases with 87% accuracy, visualized data on Tableau, and presented the results to SMEs."
                 ]}
             />
         </section>
