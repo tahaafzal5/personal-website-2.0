dd042ba33fc07478b63516843b487e1308f08558
Taha Afzal <tahaafzal5@hotmail.com>
2023-02-03 23:45:05 -0600

    Add Helmet Title to 404 Page

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./NotFound.css";
+import Helmet from "react-helmet";
 
 function NotFound() {
     return (
         <section class="not-found">
+            <Helmet>
+                <title>404</title>
+            </Helmet>
+
             <h1 class="heading">404: Page not found</h1>
             <p>The rabbits might be nibbling on the cables again...</p>
         </section>
