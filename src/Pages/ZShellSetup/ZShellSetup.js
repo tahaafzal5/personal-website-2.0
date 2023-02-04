6f503b8fd83e5ca64af9271f9c984576c87e474e
Taha Afzal <tahaafzal5@hotmail.com>
2023-02-03 18:59:37 -0600

    Remove Unnecessary Import

--- src/Pages/ZShellSetup/ZShellSetup.js
+++ src/Pages/ZShellSetup/ZShellSetup.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Helmet from "react-helmet";
-import { Code, CodeBlock } from "react-code-blocks";
+import { CodeBlock } from "react-code-blocks";
 import "./ZShellSetup.css";
 
 function ZShellSetup() {
