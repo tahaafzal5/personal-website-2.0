173bb859dac40385d2a1b402ac76bebf13f3ee95
Taha Afzal <tahaafzal5@hotmail.com>
2023-12-23 14:55:24 -0600

    Updated Leadership and Affiliations on Resume Tab

--- src/Components/LeadershipAndAffiliations/LeadershipAndAffiliations.js
+++ src/Components/LeadershipAndAffiliations/LeadershipAndAffiliations.js
@@ -7,6 +7,16 @@ function LeadershipAndAffiliations() {
             <h1 className="heading">LEADERSHIP AND AFFILIATIONS</h1>
 
             <LeadershipOrAffiliation
+                organization="UnderDog Devs"
+                role="Mentor"
+                date="Dec 2023 - Present"
+                description={[
+                    "Serving as a mentor to aspiring developers who are either formerly incarcerated or from an economically disadvantaged background.",
+                    "Conducting weekly meetings with mentees to offer coding practice, interview preparation, resume analysis, and encouragement."
+                ]}
+            />
+
+            <LeadershipOrAffiliation
                 organization="Office of Diversity and Inclusion"
                 role="Diversity Peer Mentor"
                 date="May 2020 - Aug 2021"
@@ -23,25 +33,6 @@ function LeadershipAndAffiliations() {
                 description={[
                     "Worked with students of Augustana and members of the community to provide a platform for the launch of 3 sustainable businesses.",
                     "Achieved a 250% increment in the participation with Augustana's team placing in the top 6 in the U.S. from 5000+ teams.",
-                ]}
-            />
-
-            <LeadershipOrAffiliation
-                organization="CodePath"
-                role="Developer"
-                date="Sep 2020 – Dec 2020"
-                description={[
-                    "Selected to participate in CodePath's Fall 2020 program from over 1000 candidates in the U.S. to learn iOS app development.",
-                    "Re-created apps like IMDb, Twitter, Instagram, and Mario Kart to build my portfolio and network.",
-                ]}
-            />
-
-            <LeadershipOrAffiliation
-                organization="Augustana eSports Club"
-                role="Founder and President"
-                date="Sep 2019 - May 2021"
-                description={[
-                    "Founded the first eSports club at Augustana College to provide a platform for students to compete in tournaments and build their network.",
                 ]}
             />
         </section >
