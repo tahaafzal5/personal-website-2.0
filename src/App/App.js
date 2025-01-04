ff0f78faf0de5f6a0b2153cae6c83d5aa386a8fd
Taha Afzal <tahaafzal5@hotmail.com>
2025-01-04 14:37:19 -0600

    Added books read in 2024

--- src/App/App.js
+++ src/App/App.js
@@ -6,6 +6,7 @@ import GettingHired from '../Pages/GettingHired/GettingHired.js';
 import BooksRead2021 from '../Pages/BooksRead/BooksRead2021.js';
 import BooksRead2022 from '../Pages/BooksRead/BooksRead2022.js';
 import BooksRead2023 from '../Pages/BooksRead/BooksRead2023.js';
+import BooksRead2024 from '../Pages/BooksRead/BooksRead2024.js';
 import MyZShellSetup from '../Pages/MyZShellSetup/MyZShellSetup.js';
 import ReleaseNotes from '../Pages/ReleaseNotes/ReleaseNotes.js';
 import DouglasPeucker from '../Pages/DouglasPeucker/DouglasPeucker.js';
@@ -29,6 +30,7 @@ function App() {
         <Route exact path='/writings/books-read-2021' element={<BooksRead2021 />}></Route >
         <Route exact path='/writings/books-read-2022' element={<BooksRead2022 />}></Route >
         <Route exact path='/writings/books-read-2023' element={<BooksRead2023 />}></Route >
+        <Route exact path='/writings/books-read-2024' element={<BooksRead2024 />}></Route >
         <Route exact path='/writings/my-zshell-setup' element={<MyZShellSetup />}></Route >
         <Route exact path='/writings/douglas-peucker' element={<DouglasPeucker />}></Route >
         <Route exact path='/writings/apple-vision-pro' element={<AppleVisionPro />}></Route>
