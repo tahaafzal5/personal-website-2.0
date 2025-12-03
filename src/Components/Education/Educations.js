745199427ccabc0bf2575aca1570fea999a71606
Taha Afzal <tahaafzal5@hotmail.com>
2025-12-02 21:48:12 -0600

    Update education with masters

--- src/Components/Education/Educations.js
+++ src/Components/Education/Educations.js
@@ -5,9 +5,14 @@ function Educations() {
     return (
         <section className="educations">
             <h1 className="heading">EDUCATION</h1>
+            <Education
+                degree="M.S. Computer Science"
+                institution="Georgia Institute of Technology"
+                date="Jan 2026 - Present"
+            />
 
             <Education
-                degree="BA Computer Science and Software Engineering"
+                degree="B.A. Computer Science and Software Engineering"
                 institution="Augustana University"
                 date="Aug 2018 - May 2022"
                 gpa="GPA: 3.75"
